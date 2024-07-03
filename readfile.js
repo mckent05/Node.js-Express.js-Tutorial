ab9273d07bf1cf8927424aa37e6aed5adba2ae8d
mckent05 <atemitope95@gmail.com>
2024-07-03 23:17:23 +0100

    Added methods for reading files

--- readfile.js
+++ readfile.js
@@ -1,9 +1,12 @@
-const { readFileSync, writeFileSync} = require("fs")
+const { readFileSync, writeFileSync } = require("fs");
 
-const firstFile = readFileSync("./Content/Subfolder/firstText.txt", "utf8")
-const secondFile = readFileSync("./Content/Subfolder/secondfile.txt", "utf8")
+const firstFile = readFileSync("./Content/Subfolder/firstText.txt", "utf8");
+const secondFile = readFileSync("./Content/Subfolder/secondfile.txt", "utf8");
 
-const newfile = writeFileSync("./Content/Subfolder/written-file", `This is the new written file: ${firstFile}, ${secondFile}` )
+writeFileSync(
+  "./Content/Subfolder/written-file",
+  `This is the new written file: ${firstFile}, ${secondFile}`
+);
 
-console.log(firstFile, secondFile)
-console.log(newfile)
+console.log(firstFile, secondFile);
+console.log(newfile);
